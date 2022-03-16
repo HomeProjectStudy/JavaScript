acc3cea58cb9e465a0e99e9f8878dc164feb007d
Gabriel Jesus <62946928+Gabriel-Jesusvix@users.noreply.github.com>
2022-03-16 12:48:43 -0300

    exercicios com nodeList

---
+++ LogicaAlgoritmos/NodeList/Template/js/main.js
@@ -0,0 +1,13 @@
+const paragraph = document.querySelector(".paragrafos");
+const ps = document.querySelectorAll("p");
+
+const style = getComputedStyle(document.body);
+const backgroundColorBody = style.backgroundColor;
+let textColor = style.color;
+
+//rgb(17, 86, 102)
+for (let p of ps) {
+  textColor = "#FFFF";
+  p.style.color = textColor;
+  p.style.backgroundColor = backgroundColorBody;
+}
